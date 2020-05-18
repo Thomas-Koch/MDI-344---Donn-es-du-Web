78fd460875a63f5446e0315e937adbca6297b3fa
Thomas <thomas.koch.ea2011@gmail.com>
2020-05-18 11:56:18 +0200

    Final version for TP2

--- 2. Philosophie/philosophie.py
+++ 2. Philosophie/philosophie.py
@@ -3,16 +3,14 @@
 
 from flask import Flask, render_template, session, request, redirect, flash
 from getpage import getPage
-# from getpage import randomSearch
-import pandas as pd
+
 
 app = Flask(__name__)
 
 app.secret_key = "96NZnk)v6h?F3P["
 
 global table_content
-df = pd.read_csv('score.txt', sep=",", header=None)
-df.columns = (["Page", "Score", "Auto"])
+    
 
 global val
 
@@ -46,7 +44,7 @@ def game() :
             d.write(str(str(session['article']) + ',' + str(session['score']) + ',' + 'False')+'\n')
             d.close()
             
-        flash("Bravo, vous avez gagné! Votre score est de : " + str(session['score']), "Won")
+        flash("Bravo, vous avez gagné ! Votre score est de : " + str(session['score']), "Won")
         return redirect('/')
         
     if session['title'] == None :
@@ -62,23 +60,6 @@ def game() :
         return render_template('game.html', title=session['title'], hrefs=session['hrefs'] )
 
 
-# #-------------------- AUTOGAME --------------------
-# @app.route('/autogame', methods=['GET'])
-# def autogame() :
-    
-#     session['score_auto'], session['path_auto'] = randomSearch(session['article'])
-#     if session['score_auto'] == None :
-#         flash("Dommage, c'est perdu... La page demandée n'existe pas.", "Lost")
-#     else :
-#         flash("La page a été trouvée en : " + str(session['score_auto']) + " étapes.", "Won")
-#         flash("La chemin trouvé est : " + ', '.join([str(s) for s in (session['path_auto'])]), "Won")
-    
-#     with open("score.txt", "a") as d:
-#         d.write(str(str(session['article']) + ',' + str(session['score_auto']) + ',' + 'True')+'\n')
-#         d.close()
-
-#     return redirect('/')
-
 
 #-------------------- MOVE --------------------
 @app.route('/move', methods=['POST'])
@@ -90,18 +71,7 @@ def move() :
         flash("Vous jouez sur plusieurs onglets.", "Lost")
         return redirect('/')
 
-    
-#-------------------- HIST --------------------
-@app.route('/hist', methods=("POST", "GET"))
-def html_table():
-    
-    return render_template('hist.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
-    
-
-#-------------------- TUTO --------------------
-@app.route('/tuto')
-def tuto():
-    return render_template('tuto.html')
+   
 
 
 if __name__ == '__main__':
